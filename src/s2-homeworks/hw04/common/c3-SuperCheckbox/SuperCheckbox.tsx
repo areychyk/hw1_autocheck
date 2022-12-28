92bd07b58478087d2ac10820c0706bef9bb57222
Ariiychyk Denis <areychyk@mail.ru>
2022-12-29 00:05:05 +0300

    hw4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,7 +27,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-
+        onChange?.(e)
         onChangeChecked?.(e.currentTarget.checked)
 
     }
