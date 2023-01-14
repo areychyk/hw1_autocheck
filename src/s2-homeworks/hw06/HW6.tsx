5ac3aa4863ddc401452f89c1d1a510dde6cd466d
Ariiychyk Denis <areychyk@mail.ru>
2023-01-14 22:12:35 +0300

    hw06

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -6,7 +6,8 @@ import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
 
 /*
- * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack
+ * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack++
+ *
  * 2 - дописать логику функции restore
  * 3 - сделать стили в соответствии с дизайном
  */
@@ -18,6 +19,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
+        setValue(restoreState<string>('hw6-editable-span-value',''))
         // делают студенты
 
     }
