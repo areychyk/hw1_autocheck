ba5baaad6cf063d9862f35e2f7eff6b47bd817f0
Ariiychyk Denis <areychyk@mail.ru>
2023-01-28 12:58:17 +0300

    hw08

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -30,14 +30,7 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             return state
         case 'check': {
 
-            return [...state].sort((a,b)=>{
-                if(a.name<b.name){
-                    return 1
-                }else{
-                    return -1
-                }
-
-            }).filter(u => u.age >= action.payload)// need to fix
+            return state.filter(u => u.age >= action.payload)// need to fix
         }
         default:
             return state
