5b727a47ebc37ce48f04e73dc2256a46b8311ab9
Ariiychyk Denis <areychyk@mail.ru>
2023-02-11 21:32:42 +0300

    hw10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,10 @@
 import s from './Loader.module.css'
+import preloader from './Rolling-2.9s-227px.svg'
+// export const Loader = () => <div className={s.loader}/>
+ export const Loader = () => <img src={preloader}/>
 
-export const Loader = () => <div className={s.loader}/>
+
+
+
+
+
